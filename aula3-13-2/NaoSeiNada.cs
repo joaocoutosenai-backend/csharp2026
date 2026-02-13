6fc9daf7d61aa86474bc7f6abd896d3e7d96e5bc
joaocoutosenai.backend <joaocouto.senai@gmail.com>
2026-02-13 19:25:01 -0300

    adicao-linha24

--- aula3-13-2/NaoSeiNada.cs
+++ aula3-13-2/NaoSeiNada.cs
@@ -21,4 +21,5 @@ Console.WriteLine("A média é: " + media);
 // Soma os valores de x e y e divide por 2.0 para calcular a média aritmética.
 // O 2.0 garante que a divisão seja feita com número decimal.
 //Console.WriteLine("A média é: " + media); 
+// Double define o tipo das variáveis x, y e media, permitindo armazenar números decimais.
 //
